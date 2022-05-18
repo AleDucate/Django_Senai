a1f56c1725cd1661584b717e2c96948feb6cf294
AleDucate <alexandre.ducate@hotmail.com>
2022-05-18 14:03:20 -0300

    adicionado super user e subir todos os modelos

--- rh/admin.py
+++ rh/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from rh.models import Departamento, Funcionario
+
+from .models import *
 # Register your models here.
+admin.site.register(Funcionario)
+admin.site.register(Departamento)
+admin.site.register(Cargo)
+admin.site.register(NotaFiscal)
+admin.site.register(ItemNotaFiscal)
+admin.site.register(Empresa)
+admin.site.register(FolhaPagamento)
+admin.site.register(Ponto)
